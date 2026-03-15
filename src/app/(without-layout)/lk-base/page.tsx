f2704948348435cc57214ca293d5e4564fa1343c
tonycorejz <mr.korovin-mihail@ya.ru>
2026-03-15 19:15:09 +0700

    Изменил отображение 1с виджетов

--- src/app/(without-layout)/lk-base/page.tsx
+++ src/app/(without-layout)/lk-base/page.tsx
@@ -1,9 +1,11 @@
 import { Fit1CWidget } from "@/components/fit1c-widget";
+import Script from "next/script";
 
 export default function LkBase() {
   return (
     <div className="">
-      <Fit1CWidget kind="lk" />
+      <div data-get-fit-index-lk>Личный кабинет работает на <a href="https://www.fitness1c.ru">1С:Фитнес клуб</a></div>
+      <Script src="https://reservi.ru/widget-fit1c.v2/js/config.js" data-fit-salon-id="288a1229-2774-4bdf-be32-76685b618ade"></Script>
     </div>
   );
 }
--- src/widgets/header/ui/additionsl-menu-links.tsx
+++ src/widgets/header/ui/additionsl-menu-links.tsx
@@ -6,7 +6,7 @@ interface Props {
     onLinkClick?: () => void;
 }
 
-export const AdditionalMenuLinks: React.FC<Props> = ({schedulClassName, lkClassName, onLinkClick}) =>{
+export const AdditionalMenuLinks: React.FC<Props> = ({ schedulClassName, lkClassName, onLinkClick }) => {
     return (
         <>
             <Link onClick={onLinkClick} href='/schedule' className={`flex items-center font-sans font-bold text-sm px-3 py-2 ring-2 ring-inset ring-[#24A746] rounded-3xl hover:bg-[#9BCEA8] group ${schedulClassName}`}>
@@ -15,9 +15,9 @@ export const AdditionalMenuLinks: React.FC<Props> = ({schedulClassName, lkClassN
                 </svg>
                 <p className="font-sans font-bold text-sm group-hover:text-white">Расписание</p>
             </Link>
-            <Link onClick={onLinkClick} href='/lk' className={`flex items-center px-3 py-2 ring-2 ring-inset ring-[#24A746] rounded-3xl hover:bg-[#9BCEA8] group ${lkClassName}`}>
+            <Link onClick={onLinkClick} href='/lk-base' className={`flex items-center px-3 py-2 ring-2 ring-inset ring-[#24A746] rounded-3xl hover:bg-[#9BCEA8] group ${lkClassName}`}>
                 <svg width="29" height="29" viewBox="0 0 29 29" xmlns="http://www.w3.org/2000/svg" className="me-2 w-5 fill-[#9BCEA8] group-hover:fill-white" >
-                    <path d="M19.3333 28.1711C17.8205 28.7076 16.1941 29 14.5 29C12.8059 29 11.1795 28.7076 9.66667 28.1711V25.375C9.66667 22.7094 11.8344 20.5417 14.5 20.5417C17.1656 20.5417 19.3333 22.7094 19.3333 25.375V28.1711ZM14.5 8.45833C13.1672 8.45833 12.0833 9.54221 12.0833 10.875C12.0833 12.2078 13.1672 13.2917 14.5 13.2917C15.8328 13.2917 16.9167 12.2078 16.9167 10.875C16.9167 9.54221 15.8328 8.45833 14.5 8.45833ZM29 14.5C29 19.8565 26.0807 24.5425 21.75 27.0534V25.375C21.75 21.3766 18.4984 18.125 14.5 18.125C10.5016 18.125 7.25 21.3766 7.25 25.375V27.0534C2.91933 24.5425 0 19.8565 0 14.5C0 6.50446 6.50446 0 14.5 0C22.4955 0 29 6.50446 29 14.5ZM19.3333 10.875C19.3333 8.20942 17.1656 6.04167 14.5 6.04167C11.8344 6.04167 9.66667 8.20942 9.66667 10.875C9.66667 13.5406 11.8344 15.7083 14.5 15.7083C17.1656 15.7083 19.3333 13.5406 19.3333 10.875Z"/>
+                    <path d="M19.3333 28.1711C17.8205 28.7076 16.1941 29 14.5 29C12.8059 29 11.1795 28.7076 9.66667 28.1711V25.375C9.66667 22.7094 11.8344 20.5417 14.5 20.5417C17.1656 20.5417 19.3333 22.7094 19.3333 25.375V28.1711ZM14.5 8.45833C13.1672 8.45833 12.0833 9.54221 12.0833 10.875C12.0833 12.2078 13.1672 13.2917 14.5 13.2917C15.8328 13.2917 16.9167 12.2078 16.9167 10.875C16.9167 9.54221 15.8328 8.45833 14.5 8.45833ZM29 14.5C29 19.8565 26.0807 24.5425 21.75 27.0534V25.375C21.75 21.3766 18.4984 18.125 14.5 18.125C10.5016 18.125 7.25 21.3766 7.25 25.375V27.0534C2.91933 24.5425 0 19.8565 0 14.5C0 6.50446 6.50446 0 14.5 0C22.4955 0 29 6.50446 29 14.5ZM19.3333 10.875C19.3333 8.20942 17.1656 6.04167 14.5 6.04167C11.8344 6.04167 9.66667 8.20942 9.66667 10.875C9.66667 13.5406 11.8344 15.7083 14.5 15.7083C17.1656 15.7083 19.3333 13.5406 19.3333 10.875Z" />
                 </svg>
                 <p className="font-sans font-bold text-sm group-hover:text-white">Личный кабинет</p>
             </Link>
