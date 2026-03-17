0b77c86fc9f2ea3ed994b9454f346a3b74231040
tonycorejz <mr.korovin-mihail@ya.ru>
2026-03-17 22:02:05 +0700

    Изменил ссылку на виджет личного кабинета

--- src/app/(without-layout)/lk-base/page.tsx
+++ src/app/(without-layout)/lk-base/page.tsx
@@ -5,7 +5,9 @@ export default function LkBase() {
   return (
     <div className="">
       <div data-get-fit-index-lk>Личный кабинет работает на <a href="https://www.fitness1c.ru">1С:Фитнес клуб</a></div>
-      <Script src="https://reservi.ru/widget-fit1c.v2/js/config.js" data-fit-salon-id="288a1229-2774-4bdf-be32-76685b618ade"></Script>
+      <Script
+        src="https://reservi.ru/widget-fit1c.v4/public/init.js" data-fit-apikey="288a1229-2774-4bdf-be32-76685b618ade" data-theme='light'>
+      </Script>
     </div>
   );
 }
